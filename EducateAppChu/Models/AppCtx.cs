007a4b79dff44a964c236731030e902b910b6c48
Чуракова Елизавета <lizuha-lis2-2002@mail.ru>
2021-10-27 10:23:38 +0500

    Добавление регистрации пользователя

--- EducateAppChu/Models/AppCtx.cs
+++ EducateAppChu/Models/AppCtx.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EducateAppChu.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EducateApp.Models

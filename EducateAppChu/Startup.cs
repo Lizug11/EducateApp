25fdb7a2c02f0be47822a8b5cd776f2e65352ac1
Чуракова Елизавета <lizuha-lis2-2002@mail.ru>
2021-10-28 12:03:04 +0500

    Добавление валидатора пароля

--- EducateAppChu/Startup.cs
+++ EducateAppChu/Startup.cs
@@ -1,5 +1,6 @@
 using EducateApp.Models;
 using EducateAppChu.Models;
+using EducateAppChu.Models.Validators;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -24,6 +25,13 @@ namespace EducateAppChu
         {
             services.AddDbContext<AppCtx>(options =>
                       options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // ƒобавление сервиса валидатора парол€
+            services.AddTransient<IPasswordValidator<User>, CustomPasswordValidator>(
+                serv => new CustomPasswordValidator(8));
+
+            // ƒобавление сервиса валидатора пользовател€
+            services.AddTransient<IUserValidator<User>, CustomUserValidator>();
 
             services.AddIdentity<User, IdentityRole>()
                 .AddEntityFrameworkStores<AppCtx>();
